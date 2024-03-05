89058df0d16eecbc3ff727d7bc30259773797a08
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-05 09:19:34 -0600

    added basic css to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import { NavLink } from "react-router-dom"
+import "../css/Navbar.css"
 const Navbar: React.FC = () => {
       return (
             <nav className="navbar">
-              <NavLink to="/" className="nav-link">HomePage</NavLink>
-              <NavLink to="/aboutme" className="nav-link">About Me</NavLink>
-              <NavLink to="/projects" className="nav-link">Projects</NavLink>
-              <NavLink to="/contact" className="nav-link">Contact</NavLink>
+              <NavLink to="/" className="nav-link">
+                HomePage
+                </NavLink>
+              <NavLink to="/aboutme" className="nav-link">
+                About Me
+                </NavLink>
+              <NavLink to="/projects" className="nav-link">
+                Projects
+                </NavLink>
+              <NavLink to="/contact" className="nav-link">
+                Contact
+                </NavLink>
             </nav>
       ); 
     }
