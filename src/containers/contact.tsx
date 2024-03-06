e54891a962522eca666ca799c593314c73307dd4
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-06 08:44:15 -0600

    added basic to contact

--- src/containers/contact.tsx
+++ src/containers/contact.tsx
@@ -1,6 +1,18 @@
 const Contact: React.FC = () => {
-    return (<h1>This is my Contacts!</h1>)
-
+    return (
+        <><><><><><h1>Thanks for looking at my Portfolio</h1><br /></>
+            // stuff explaining my thoughts
+            <h2>Let Talk!</h2>
+        </><br /></>
+            <p>Name</p>
+            <br />
+            <input type="text" /></><p>Your Email Address</p>
+            <input type="text" />
+            <p>Message</p>
+            <input type="text" />
+            <br/>
+            <button>submit</button></>
+  )
 }
 
     export default Contact;
