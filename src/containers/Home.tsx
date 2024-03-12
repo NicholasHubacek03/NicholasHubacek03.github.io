fa43a8e89bdbdf6e2df2fbf2b5dea9be5a70e9b4
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-12 13:33:12 -0500

    updated aboutus

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
     return (
         <><h1>Hi There! I'm Nicholas</h1>
         <h1>An full stack Developer</h1>
-        <p>An Full Stack Developer</p>
+        <p>An Full Stack Developer. Passionte and always learning dev</p>
         {/* <img src="src/pictures/NicholasHubacek.png" alt="Nicholas"></img> */}
         <a href="https://docs.google.com/document/d/1XWwixWkrz3NSW3eM3BB0Gr3tBJ7y6CceQ22B1EQJaqo/edit?usp=sharing">
             <button>Resume</button></a>
--- src/containers/aboutme.tsx
+++ src/containers/aboutme.tsx
@@ -1,6 +1,13 @@
-const AboutMe: React.FC = () => {
-    return (<h1>This is my AboutMe!</h1>)
+import React from 'react';
 
+const AboutMe: React.FC = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Welcome to my personal portfolio! I am a passionate developer with experience in web development.</p>
+            <p>Feel free to explore my projects and get in touch with me.</p>
+        </div>
+    );
 }
 
-    export default AboutMe;
+export default AboutMe;
