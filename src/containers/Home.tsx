1b09d2b01b252fb38a52db2cd38debdcad5c8536
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-13 16:51:21 -0500

    finsih up on homepage

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -3,8 +3,10 @@ import Tooling from "../components/Tooling";
 const Home: React.FC = () => {
     return (
         <><h1>Hi There! I'm Nicholas</h1>
-        <h1>An full stack Developer</h1>
-        <p>An Full Stack Developer. Passionte and always learning dev</p>
+        <h1>Software Developer</h1>
+        <p>Hi and Welcome to my portfolio! I'm an passionate full stack developer with dedication of creating clean code and experience problem solving skills.
+            I have experience in web development and I am always looking to learn and grow as a developer. Feel free to explore my projects and get in touch with me. 
+        </p>
         {/* <img src="src/pictures/NicholasHubacek.png" alt="Nicholas"></img> */}
         <a href="https://docs.google.com/document/d/1XWwixWkrz3NSW3eM3BB0Gr3tBJ7y6CceQ22B1EQJaqo/edit?usp=sharing">
             <button>Resume</button></a>
