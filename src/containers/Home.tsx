3d808ba4586c90c9dc617f0af4fbb8ec35b888d9
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-25 14:17:49 -0500

    adjusting the css

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -6,12 +6,12 @@ const Home: React.FC = () => {
     return (
         <><br /><div className="home-container">
             <div className="home-header">
-                <h1>Hi There! I'm Nicholas</h1>
-                <h1>Software Developer</h1>
+                <h1 className="First">Hi There! I'm Nicholas</h1>
+                <h1 className="Second">Software Developer</h1>
             </div>
-            <p className="home-intro">
+            <h3 className="home-intro">
                 Hi and Welcome to my portfolio! I'm a passionate full stack developer dedicated to creating clean code and solving problems. With experience in web development, I'm always eager to learn and grow as a developer. Feel free to explore my projects and get in touch with me.
-            </p>
+            </h3>
             <div className="home-actions">
                 <img src="src/pictures/NicholasHubacek remaster.png" alt="Nicholas" className="home-image" />
                 <a href="https://docs.google.com/document/d/1XWwixWkrz3NSW3eM3BB0Gr3tBJ7y6CceQ22B1EQJaqo/edit?usp=sharing">
