baba27de1d8681080c3f557690dc0b255b160b31
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-26 15:51:57 -0500

    added fonts

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,29 +1,32 @@
 import Projects from "../components/Projects";
 import Tooling from "../components/Tooling";
 import "../css/Home.css";
+import React from "react";
 
 const Home: React.FC = () => {
     return (
-        <><br /><div className="home-container">
-            <div className="home-header">
-                <h1 className="First">Hi There! I'm Nicholas</h1>
-                <h1 className="Second">Software Developer</h1>
+        <> <br />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" />
+            <div className="home-container">
+                <div className="home-header">
+                    <h1 className="First">Hi There! I'm Nicholas</h1>
+                    <h1 className="Second">Software Developer</h1>
+                </div>
+                <h3 className="home-intro">
+                    Hi and Welcome to my portfolio! I'm a passionate full stack developer dedicated to creating clean code and solving problems. With experience in web development, I'm always eager to learn and grow as a developer. Feel free to explore my projects and get in touch with me.
+                </h3>
+                <div className="home-actions">
+                    <img src="src/pictures/NicholasHubacek remaster.png" alt="Nicholas" className="home-image" />
+                    <a href="https://docs.google.com/document/d/1XWwixWkrz3NSW3eM3BB0Gr3tBJ7y6CceQ22B1EQJaqo/edit?usp=sharing">
+                        <button className="home-button">Resume</button>
+                    </a>
+                </div>
+                <div>
+                    <Tooling />
+                </div>
+                <Projects />
             </div>
-            <h3 className="home-intro">
-                Hi and Welcome to my portfolio! I'm a passionate full stack developer dedicated to creating clean code and solving problems. With experience in web development, I'm always eager to learn and grow as a developer. Feel free to explore my projects and get in touch with me.
-            </h3>
-            <div className="home-actions">
-                <img src="src/pictures/NicholasHubacek remaster.png" alt="Nicholas" className="home-image" />
-                <a href="https://docs.google.com/document/d/1XWwixWkrz3NSW3eM3BB0Gr3tBJ7y6CceQ22B1EQJaqo/edit?usp=sharing">
-                    <button className="home-button">Resume</button>
-                </a>
-            </div>
-            <div>
-                
-                <Tooling />
-            </div>
-            <Projects />
-        </div></>
+        </>
     );
 }
 
