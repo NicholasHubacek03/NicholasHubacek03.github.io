771b55c80fd2d1879413eaaee367af5021e6d0b0
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-03-29 11:52:43 -0500

    trying delopying

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar: React.FC = () => {
     return (
         <nav id="nav">
            <ul className="container">
-                <li><Link to="top" smooth={true} duration={2000} >Introduction</Link></li>
-                <li><Link to="portfolio" smooth={true} duration={2000} >Projects</Link></li>
-                <li><Link to="contact" smooth={true} duration={2000} >Contact</Link></li>
+                <li><Link to="top" smooth={true} duration={1500} >Introduction</Link></li>
+                <li><Link to="portfolio" smooth={true} duration={1500} >Projects</Link></li>
+                <li><Link to="contact" smooth={true} duration={1500} >Contact</Link></li>
             </ul>
         </nav>
     );
