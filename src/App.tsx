73c06f01fc71d6e8dfbbef1bdfb4ad2265daa75b
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-04-02 08:58:47 -0500

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Projects from "./containers/projects";
-import Contact from "./containers/contact";
 import Home from "./containers/Home";
 
 class App extends React.Component {
   render() {
     return (
-    <Router>
       <div>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-
-         {}
-         
+        <Home />
       </div>
-    </Router>
     );
   }
-  
 }
 
 export default App;
