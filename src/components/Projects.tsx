9332af070e57dc3b00db06e7fd73202ba0cdb551
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-04-10 13:21:37 -0500

    fixed images hopefully

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 import '../css/Projects.css';
 import DailyDripImg from '../../public/pictures/projects/The Daily Drip!.jpg'
+import WeatherProjectImg from '../../public/pictures/projects//Weather-project.jpg'
+import TrakFlixImg from '../../public/pictures/projects/Trakflix.jpg'
+import CommuniListImg from '../../public/pictures/projects/CommuniList.jpg'
+import BrewQuestImg from '../../public/pictures/projects/BrewQuest.jpg'
+import ComingSoonImg from '../../public/pictures/projects/coming soon.jpg'
 
 const Projects: React.FC = () => {
     const projects = [
@@ -9,19 +14,19 @@ const Projects: React.FC = () => {
             title: 'BrewQuest',
             description: "Allowing users to explore breweries, share reviews, participate as drivers, and earn badges. This engaging platform combines a passion for breweries with a rewarding user experience.",
             button: <a href='https://github.com/BrewQuest/BrewQuest'>Repo</a>,
-            image: <img src="src/pictures/projects/BrewQuest.jpg" alt="BrewQuest"></img>
+            image: <img src={BrewQuestImg} alt="BrewQuest"></img>
         },
         {
             title: 'Weather Monitor',
             description: "An application to track the 5-day forecast for any city in the world, providing comprehensive weather information.",
             button: <a href='https://github.com/NicholasHubacek03/codeup-web-exercises/blob/main/weather_map.html'>Repo</a>,
-            image: <img src="src/pictures/projects/Weather-project.jpg" alt="Weather Monitor"></img>
+            image: <img src={WeatherProjectImg} alt="Weather Monitor"></img>
         },
         {
             title: 'TrakFlix',
             description: 'Trakflix is a movie application that operates on a single page, providing interactive features and complete CRUD functionality (Create, Read, Update, and Delete) to manage data within a locally stored JSON database using FETCH requests.',
             button: <a href='https://github.com/francis-hubacek-movies-app/TrakFlix'>Repo</a>,
-            image: <img src="src/pictures/projects/Trakflix.jpg" alt="Trakflix"></img>
+            image: <img src={TrakFlixImg} alt="Trakflix"></img>
         },
         {
             title: 'The Daily Drip',
@@ -33,14 +38,14 @@ const Projects: React.FC = () => {
             title: 'CommuniList',
             description: 'An application offers users a seamless experience, allowing them to register an account, log out, and efficiently modify their account details. Additionally, enabling users to effortlessly add, edit, or delete ads to their page.',
             button: <a href='https://github.com/autrey-hubacek-gutierrez-adlister/a-n-m-adlister'>Repo</a>,
-            image: <img src="src/pictures/projects/CommuniList.jpg" alt="CommuniList"></img>
+            image: <img src={CommuniListImg} alt="CommuniList"></img>
 
         },
         {
             title: 'SwiftList',
             description: ' Your all-in-one calendar companion, offering seamless integration with Google API for daily, weekly, and monthly tracking, to-dos, and much more.',
             button: <a href='https://github.com/NicholasHubacek03/SwiftList_web'>Repo</a>,
-            image: <img src="src/pictures/projects/coming soon.jpg" alt="Coming Soon"></img>
+            image: <img src={ComingSoonImg} alt="Coming Soon"></img>
 
         }
     ];
--- src/components/Tooling.tsx
+++ src/components/Tooling.tsx
@@ -1,43 +1,55 @@
 import "../css/Tooling.css";
+import JavaImg from '../../public/icons/java-icon.svg'
+import SringSvg from '../../public/icons/spring-3.svg'
+import JqureyImg from '../../public/icons/jquery-icon.svg'
+import JavaScriptImg from '../../public/icons/logo-javascript.svg'
+import BootStrapImg from '../../public/icons/bootstrap-5-1.svg'
+import NpmImg from '../../public/icons/npm.svg'
+import NestJsImg from '../../public/icons/nestjs-icon.svg'
+import NodeJsImg from '../../public/icons/nodejs-icon.svg'
+import ReactImg from '../../public/icons/react-2.svg'
+import TypeScriptImg from '../../public/icons/typescript.svg'
+import ViteImg from '../../public/icons/vitejs.svg'
+import GitImg from '../../public/icons/git-icon.svg'
 
 const Tooling: React.FC = () => {
     return (
         <><h2>Tools</h2><div className="Tooling">
             <div className="tool">
-            <img src="src/pictures/icons/java-icon.svg" alt="Java" /> Java
+            <img src={JavaImg} alt="Java" /> Java
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/spring-3.svg" alt="Spring Boot" /> Spring Boot
+            <img src={SringSvg} alt="Spring Boot" /> Spring Boot
             </div>
             <div className="tool">
-                <img src="src/pictures/icons/jquery-icon.svg" alt="jQuery" /> jQurey
+                <img src={JqureyImg} alt="jQuery" /> jQurey
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/logo-javascript.svg" alt="JavaScript" /> JavaScript
+            <img src={JavaScriptImg} alt="JavaScript" /> JavaScript
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/bootstrap-5-1.svg" alt="Bootstrap" /> Bootstrap
+            <img src={BootStrapImg} alt="Bootstrap" /> Bootstrap
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/npm.svg" alt="npm" /> npm
+            <img src={NpmImg} alt="npm" /> npm
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/nestjs-icon.svg" alt="Nest.js" /> Nest.js
+            <img src={NestJsImg} alt="Nest.js" /> Nest.js
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/nodejs-icon.svg" alt="Node.js" /> Node.js
+            <img src={NodeJsImg} alt="Node.js" /> Node.js
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/react-2.svg" alt="React" /> React
+            <img src={ReactImg} alt="React" /> React
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/typescript.svg" alt="Typescript" /> Typescript
+            <img src={TypeScriptImg} alt="Typescript" /> Typescript
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/vitejs.svg" alt="Vite" /> Vite
+            <img src={ViteImg} alt="Vite" /> Vite
             </div>
             <div className="tool">
-            <img src="src/pictures/icons/git-icon.svg" alt="Git" /> Git
+            <img src={GitImg} alt="Git" /> Git
             </div>
         </div></>
     );
