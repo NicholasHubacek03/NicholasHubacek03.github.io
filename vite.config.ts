eba2f05cd7283495a1d2af36c79429b7cf286884
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2024-04-10 12:28:07 -0500

    deleted

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
-  },
-  base: 'raw.githubusercontent.com/NicholasHubacek03/NicholasHubacek03.github.io/main/'
+  }
 });
